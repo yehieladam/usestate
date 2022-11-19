fa5c2f65204d2ce3bed8243da6ee47cb69125fc5
Yehiel_Adam <yehieladam@gmail.com>
2022-11-19 18:12:27 +0200

    seconde lesson2

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Counter from './Counter';
+function App() {
+  return (
+    <div className="App">
+      Usestate
+  <Counter/>
+    </div>
+  );
+}
+export default App;
