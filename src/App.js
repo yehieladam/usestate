f79bcaf7c17f56ae792ea1d49e906240b95f1d06
Yehiel_Adam <yehieladam@gmail.com>
2022-11-24 18:49:02 +0200

    Exercise 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import './App.css';
+import { useState } from "react";
 import Counter from './Counter';
+
+
+
 function App() {
+
+    const[delta,setDelta] = useState(1);
+    const[limit,setLimit] = useState(1);
+
+    function limitos(l){
+      setLimit(Number(l.target.value))
+}
+
+
+    function hendeleDelta(e){
+        setDelta(Number(e.target.value))
+        if(delta >= limit)
+        setDelta("0")
+      }
+  
+
   return (
     <div className="App">
       Usestate
    
   <div className="box" >
-  <Counter/>
+  <p>Choose the maximum number for the counter is </p>
+  <input type = "number" value={limit} onChange={limitos}/> 
+    
+  <p> Counter should jump By </p><input type = "number" value={delta} onChange={hendeleDelta}/>
+
+  <Counter delta = {delta} limit ={limit} />
+  <Counter delta = {delta} limit ={limit} />
 
   </div>
     </div>
